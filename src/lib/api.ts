62c4f764ffa127d055db8248954d825ce8206a5b
Ken <yauken@outlook.com>
2025-09-25 10:27:00 -0700

    Switch to useSuspenseInfiniteQuery for paging

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 import { type PostsResponse } from "@/lib/types/post";
-import { API_BASE_URL } from "./constants";
+import { API_BASE_URL, POSTS_PER_PAGE } from "./constants";
 
-export function fetchPosts() {
+export function fetchPosts(page: number) {
   return axios
-    .get<PostsResponse>(`${API_BASE_URL}/post`)
+    .get<PostsResponse>(
+      `${API_BASE_URL}/post?limit=${POSTS_PER_PAGE}&skip=${
+        page * POSTS_PER_PAGE
+      }`
+    )
     .then((response) => response.data);
 }
