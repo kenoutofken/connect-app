e953e20e9355a0b1f8a66cb89e6646297709fe4b
Ken <yauken@outlook.com>
2025-10-30 10:05:35 -0700

    add tags to post

--- src/components/createPost/CreatePost.tsx
+++ src/components/createPost/CreatePost.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -19,8 +20,32 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import styles from "@/components/latestPosts/latestPosts.module.css";
+import type { fr } from "zod/v4/locales";
+import TagChip from "@/components/createPost/TagChip";
+
+const allTags = [
+  "american",
+  "classic",
+  "crime",
+  "english",
+  "fiction",
+  "french",
+  "history",
+  "love",
+  "magical",
+  "mystery",
+];
 
 const postSchema = z.object({
   title: z.string().min(1, "Title is required, at least 1 character long."),
@@ -28,6 +53,9 @@ const postSchema = z.object({
 });
 
 export default function CreatePost() {
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [availableTags, setAvailableTags] = useState<string[]>(allTags);
+
   const form = useForm<z.infer<typeof postSchema>>({
     resolver: zodResolver(postSchema),
     defaultValues: {
@@ -37,6 +65,26 @@ export default function CreatePost() {
   });
 
   const navigate = useNavigate();
+
+  const addTag = useCallback(
+    (tag: string) => {
+      if (tag && !selectedTags.includes(tag)) {
+        setSelectedTags([...selectedTags, tag]);
+        setAvailableTags(
+          availableTags.filter((availableTag) => tag !== availableTag)
+        );
+      }
+    },
+    [selectedTags, availableTags]
+  );
+
+  const removeTag = useCallback(
+    (tag: string) => {
+      setSelectedTags(selectedTags.filter((t) => t !== tag));
+      setAvailableTags([...availableTags, tag]);
+    },
+    [selectedTags, availableTags]
+  );
 
   return (
     <Dialog
@@ -92,6 +140,34 @@ export default function CreatePost() {
                 </FormItem>
               )}
             />
+            <div className="border border-gray-300 mb-2 px-4 pt-2 pb-3 rounded-md">
+              {selectedTags.length === 0 ? (
+                <span className="text-gray-400 cursor-default">
+                  Tags appear here
+                </span>
+              ) : (
+                selectedTags.map((tag) => {
+                  return <TagChip key={tag} tag={tag} onDelete={removeTag} />;
+                })
+              )}
+            </div>
+            <Select onValueChange={(value) => addTag(value)}>
+              <SelectTrigger className="w-48">
+                <SelectValue placeholder="Select some tags">
+                  Select some tags
+                </SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Available Tags</SelectLabel>
+                  {availableTags.map((tag) => (
+                    <SelectItem key={tag} value={tag}>
+                      {tag}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </form>
         </Form>
         <DialogFooter>
